a0a215efd7ffceab929d7769487fab8eac11da4a
valle1212i <korpela.valentin@gmail.com>
2025-07-23 22:54:50 +0200

    Ändrat models/Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
-  sender: { type: String, enum: ['admin', 'customer'], required: true },
+  sender: { type: String, enum: ['admin', 'customer', 'system'], required: true },
   message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  sessionId: { type: String, required: true } // 🔧 glöm inte denna!
 });
 
 module.exports = mongoose.model('Message', messageSchema);
