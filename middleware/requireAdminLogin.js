3dff43fa7cef80185ef464ed46195bad78b7342e
valle1212i <korpela.valentin@gmail.com>
2025-07-26 01:51:15 +0200

    ändrat i requireAdminLogin.js för att kunna logga in via render

--- middleware/requireAdminLogin.js
+++ middleware/requireAdminLogin.js
@@ -1,8 +1,9 @@
 function requireAdminLogin(req, res, next) {
     if (!req.session?.admin) {
-      return res.redirect('/admin-login.html');
+      return res.redirect('/login.html');
     }
     next();
   }
+  
   module.exports = requireAdminLogin;
   
