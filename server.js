ffd566ed283d9d15d54015d18b05608d98e46c53
valle1212i <korpela.valentin@gmail.com>
2025-08-27 19:36:34 +0200

    ändrat

--- server.js
+++ server.js
@@ -86,7 +86,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/cases", require("./routes/cases"));
 app.use("/api/admin-status", require("./routes/adminStatus"));
 app.use("/api/admins", require("./routes/admins"));
-app.use('/api/email', require('./routes/emailRoutes'));
 app.use('/api/contracts', require('./routes/contracts'));
 const { router: securityRouter, requireAdmin } = require('./routes/security');
 app.use('/api/security', securityRouter);
